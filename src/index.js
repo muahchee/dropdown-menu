8d60fc58b168c2888e10a655f2e2e8aa8991a46c
muahchee <187490843+muahchee@users.noreply.github.com>
2025-03-30 14:38:09 +1300

    add next button

--- src/index.js
+++ src/index.js
@@ -7,16 +7,23 @@ class ImageCarousel {
     this.slideContainer = slideContainer;
     this.slides = this.slideContainer.children;
     this.frameWidth = frameWidth;
-    this.currentSlide = 0
+    this.currentSlide = 1;
+
+    this.slideArr = [];
+    this.slideContainer.childNodes.forEach((slide) => {
+      if (slide.src) {
+        this.slideArr.push(slide);
+      }
+    });
   }
 
   _structure() {
-    this.frame.style.maxWidth = this.frameWidth;
-    // this.frame.style.overflow = "hidden"
+    this.frame.style.maxWidth = `${this.frameWidth}px`;
     this.frame.style.position = "relative";
 
     this.slideContainer.style.display = "flex";
     this.slideContainer.style.alignItems = "center";
+    this.slideContainer.style.overflow = "none";
 
     this.previousButton = document.createElement("button");
     this.previousButton.textContent = "←";
@@ -35,27 +42,55 @@ class ImageCarousel {
   }
 
   _numberSlides() {
-    const slideArr = [];
-    this.slideContainer.childNodes.forEach((slide) => {
-      if (slide.src) {
-        slideArr.push(slide);
+    for (let i = 1; i < this.slideArr.length + 1; i++) {
+      this.slideArr[i - 1].setAttribute("slideNumber", i);
+    }
+    return this.slideArr;
+  }
+
+  _hideImg() {
+    const currentSlideImg = this.slideContainer.querySelector(
+      `[slidenumber = "${this.currentSlide}"]`,
+    );
+    currentSlideImg.style.visibility = "visible";
+
+    this.slideArr.forEach((node) => {
+      if (
+        node.getAttribute("slidenumber") !==
+        currentSlideImg.getAttribute("slidenumber")
+      ) {
+        node.style.visibility = "hidden";
       }
     });
-    for (let i = 0; i < slideArr.length; i++){
-      slideArr[i].setAttribute("slideNumber", i);
-    }
-    return slideArr
-  };
+  }
 
   _next() {
+    if (this.currentSlide < this.slides.length) {
+      this.slideContainer.style.transform = `translateX(-${this.frameWidth * this.currentSlide}px)`;
 
- 
+      this.currentSlide += 1;
+      this._hideImg()
+      console.log(this.currentSlide);
+      return this.slideContainer;
+    }
+
+    this.currentSlide = 1;
+    this._hideImg()
+    this.slideContainer.style.transform = `translateX(0)`;
+    console.log(this.currentSlide);
   }
 
   create() {
     this._structure();
     this._numberSlides();
+    this._hideImg();
+
+    this.nextButton.addEventListener("click", () => {
+      this._next();
+    });
+
+    
   }
 }
 
-new ImageCarousel(frame, slideContainer, "300px").create();
+new ImageCarousel(frame, slideContainer, 300).create();
