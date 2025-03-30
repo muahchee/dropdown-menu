e6e3ffb7212b200a406728441fb4261278c223b3
muahchee <187490843+muahchee@users.noreply.github.com>
2025-03-30 13:32:01 +1300

    start image carousel module

--- src/index.js
+++ src/index.js
@@ -1,7 +1,61 @@
-const DropdownMenu = require("dropdown-menu");
+const slideContainer = document.querySelector(".slide-container");
+const frame = document.querySelector(".frame");
 
-const button1 = document.querySelector("button");
+class ImageCarousel {
+  constructor(frame, slideContainer, frameWidth) {
+    this.frame = frame;
+    this.slideContainer = slideContainer;
+    this.slides = this.slideContainer.children;
+    this.frameWidth = frameWidth;
+    this.currentSlide = 0
+  }
 
-const menu1 = document.querySelector("div");
+  _structure() {
+    this.frame.style.maxWidth = this.frameWidth;
+    // this.frame.style.overflow = "hidden"
+    this.frame.style.position = "relative";
 
-new DropdownMenu(button1, menu1).create();
+    this.slideContainer.style.display = "flex";
+    this.slideContainer.style.alignItems = "center";
+
+    this.previousButton = document.createElement("button");
+    this.previousButton.textContent = "←";
+    this.previousButton.style.position = "absolute";
+    this.previousButton.style.left = "-15%";
+    this.previousButton.style.top = "50%";
+
+    this.nextButton = document.createElement("button");
+    this.nextButton.textContent = "→";
+    this.nextButton.style.position = "absolute";
+    this.nextButton.style.right = "-15%";
+    this.nextButton.style.top = "50%";
+
+    this.frame.appendChild(this.previousButton);
+    this.frame.appendChild(this.nextButton);
+  }
+
+  _numberSlides() {
+    const slideArr = [];
+    this.slideContainer.childNodes.forEach((slide) => {
+      if (slide.src) {
+        slideArr.push(slide);
+      }
+    });
+    for (let i = 0; i < slideArr.length; i++){
+      slideArr[i].setAttribute("slideNumber", i);
+    }
+    return slideArr
+  };
+
+  _next() {
+
+ 
+  }
+
+  create() {
+    this._structure();
+    this._numberSlides();
+  }
+}
+
+new ImageCarousel(frame, slideContainer, "300px").create();
